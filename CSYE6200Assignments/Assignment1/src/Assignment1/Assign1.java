447e8253145147c974fb2868dd1b6b9c75635390
Dongyue Li <li.dongy@husky.neu.edu>
2017-01-25 16:43:31 -0500

    first assignment upload

---
+++ CSYE6200Assignments/Assignment1/src/Assignment1/Assign1.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment1;
+
+import java.util.Scanner;
+
+/**
+* A starter file for implementing CSYE 6200 Assignment 1
+* Filename: Assign1.java
+* NUID: 001632075
+* @author Dongyue Li
+*/
+public class Assign1 {
+    public static void main(String[] args) throws java.io.IOException {
+        String accounts[][] = {  {"dongyueli", "1575", "12000.00"},  {"ldy", "7721", "500.00"},  {"dyl",  "3884", "700.00"}};
+
+        Scanner sc = new Scanner(System.in);        
+        boolean authorized = false;
+        String authorizedUser = null;
+        int authorizedUserIndex = -1;
+        while(authorized == false){
+            System.out.println("Please enter username: ");
+            String user = sc.nextLine();
+            System.out.println("Please enter password: ");
+            try{
+                String passStr = sc.nextLine();
+                int pass = Integer.parseInt(passStr);
+                
+                for(int i = 0; i < accounts.length; i++){
+                    if(user.equals(accounts[i][0]) && pass==Integer.parseInt(accounts[i][1])){
+                        authorized = true;
+                        authorizedUser = accounts[i][0];
+                        authorizedUserIndex = i;
+                        break;
+                    }
+                }
+                if(authorized == false){
+                    System.out.println("Wrong User Name or password. Try again: ");
+                }
+            }catch(Exception e){
+                System.out.println("Number Only!");
+            }
+        }
+        
+        if(authorized == true && authorizedUser!= null && authorizedUserIndex >= 0){
+            String inChar = "";
+            do {
+                System.out.println("Welcome " + authorizedUser +" Please enter 1 for account balance, 2 for Obtain cash, 3 for quit");                
+                inChar = sc.nextLine();
+                if(inChar.equals("1") || inChar.equals("2") || inChar.equals("3")){
+                    switch(inChar){
+                        case "1": System.out.println("You have: " + accounts[authorizedUserIndex][2] + " dollars");
+                            break;
+                        case "2": 
+                            System.out.println("Please enter the amount you need: ");
+                            boolean  validInput = false;
+                            while(!validInput) {
+                                try{
+                                    int amount = Integer.parseInt(sc.nextLine());
+                                    int amountWithDraw = amount;
+                                    if(amount > Double.parseDouble(accounts[authorizedUserIndex][2]) || amount > 200){
+                                        System.out.println("Amount cannot greater than 200 for one transection! Please Enter Again !");
+                                        continue;
+                                    }
+                                    validInput = true;
+                                    if(amount % 20 == 0 && amount >= 20){
+                                        System.out.println("$20 bill: " + (amount / 20));
+                                        accounts[authorizedUserIndex][2] = String.valueOf(Double.parseDouble(accounts[authorizedUserIndex][2]) - amountWithDraw);                           
+                                        System.out.println("You are all set!");
+                                        break;
+                                    }
+                                    else if(amount >= 20){
+                                        System.out.println("$20 bill: " + (amount / 20));
+                                        amount = amount % 20;
+                                    }
+
+                                    if(amount % 10 == 0 && amount >= 10){
+                                        System.out.println("$10 bill: " + (amount / 10));
+                                        accounts[authorizedUserIndex][2] = String.valueOf(Double.parseDouble(accounts[authorizedUserIndex][2]) - amountWithDraw);                                    
+                                        System.out.println("You are all set!");
+                                        break;
+                                    }
+                                    else if(amount >= 10){
+                                        System.out.println("$10 bill: " + (amount / 10));
+                                        amount = amount % 10;
+                                    }
+
+                                    if(amount % 5 == 0 && amount >= 5){
+                                        System.out.println("$5 bill: " + (amount / 5));
+                                        accounts[authorizedUserIndex][2] = String.valueOf(Double.parseDouble(accounts[authorizedUserIndex][2]) - amountWithDraw);                                    
+                                        System.out.println("You are all set!");
+                                        break;
+                                    }
+                                    else if(amount >= 5){
+                                        System.out.println("$5 bill: " + (amount / 5));
+                                        amount = amount % 5;
+                                    }
+
+                                    if(amount > 0){
+                                        System.out.println("$1 bill: " + (amount / 1));
+                                        System.out.println("You are all set!");
+                                    }
+                                    
+                                    accounts[authorizedUserIndex][2] = String.valueOf(Double.parseDouble(accounts[authorizedUserIndex][2]) - amountWithDraw);                                    
+                                    break;                           
+                                }
+                                catch(Exception e){
+                                    System.out.println("Integer Only ! Please Enter Again !");
+                                }
+                            }
+                    }
+                } else {
+                    System.out.println("Wrong input!");
+                }
+            } 
+            while (!inChar.equals("3")); // Exit on quit
+            System.out.println("Quiting...");
+        }   
+    } 
+
+}
